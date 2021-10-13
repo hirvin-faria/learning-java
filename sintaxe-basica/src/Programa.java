a5737e3f7ed19e7ec228350190e6a6ad4909c2d3
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2021-10-13 20:21:17 -0300

    feat: adicionando projeto de sintaxe basica do java

---
+++ sintaxe-basica/src/Programa.java
@@ -0,0 +1,12 @@
+
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("olá mundo");
+		
+		int idade;
+		idade = 24;
+		
+		double salario;
+		salario = 2500.50;
+	}
+}
